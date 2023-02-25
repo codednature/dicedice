44410e455108a1c8c8c07eaa764897e30b1288f7
naturesola <100572919+naturesola@users.noreply.github.com>
2023-02-25 22:45:05 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+// Dice1
+var randomNumber1 = Math.floor (Math.random() * 6)+1
+
+var randomDiceImage = "dice" + randomNumber1 + ".png";
+
+var randomImageSource = "images/" + randomDiceImage;
+
+var image1 = document.querySelectorAll("img")[0]
+
+image1.setAttribute("src", randomImageSource)
+
+
+// Dice 2
+var randomNumber2 = Math.floor (Math.random() * 6)+1
+
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+
+var image2 = document.querySelectorAll("img")[1]
+
+image2.setAttribute("src", randomImageSource2)
+
+// If player 1 wins
+if (randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerHTML = "🚩Player 1 Wins!"
+}
+// If player 2 wins
+else if (randomNumber2 > randomNumber1) {
+    document.querySelector("h1").innerHTML = "Player 2 Wins!🚩"
+}
+// Draw
+else if (randomNumber1 === randomNumber2) {
+    document.querySelector("h1").innerHTML = "🏁Draw"
+}
